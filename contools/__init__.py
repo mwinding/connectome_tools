d11d0ca00c6f7906a89f312cf3b94677b03c2fc7
Michael Winding <mwinding@alumni.nd.edu>
2022-02-01 15:33:32 +0000

    Update __init__.py

--- contools/__init__.py
+++ contools/__init__.py
@@ -2,5 +2,4 @@ from .cascade_analysis import *
 from .celltype import *
 from .process_graph import *
 from .process_matrix import *
-from .traverse.cascade import *
-from .traverse.traverse import *
+from .traverse import *
