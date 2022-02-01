361b84457ef0b5abcc0c2078f799e01470e631af
Michael Winding <mwinding@alumni.nd.edu>
2022-02-01 13:41:23 +0000

    changed name

--- setup.py
+++ setup.py
@@ -1,5 +1,5 @@
 from distutils.core import setup
-setup(name='connectome_tools',
+setup(name='contools',
       version='0.1',
-      py_modules=['connectome_tools'],
+      py_modules=['contools'],
       )
