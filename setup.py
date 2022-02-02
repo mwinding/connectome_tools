62cbfe6686d48a9710a584eb716cb2ad0b2aa44c
mwinding <mwinding@alumni.nd.edu>
2022-02-02 12:42:43 +0000

    updated install requirements

--- setup.py
+++ setup.py
@@ -2,5 +2,11 @@ from setuptools import setup, find_packages
 
 setup(name='contools',
       version='0.1',
-      packages=find_packages(include=['contools', 'contools.*'])
+      description='tools for analyzing connectomics data, focus on Drosophila larva dataset',
+      url='http://github.com/mwinding/connectome_tools',
+      author='Michael Winding and Ben Pedigo',
+      author_email='mwinding@alumni.nd.edu',
+      license='MIT',
+      packages=find_packages(include=['contools', 'contools.*'],
+      install_requires=['tables', 'python-catmaid', 'graspy'])
       )
