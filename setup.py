ea88ed055aee27db7634f2449cb3c86407a85539
mwinding <mwinding@alumni.nd.edu>
2022-02-03 15:12:11 +0000

    updated

--- setup.py
+++ setup.py
@@ -2,9 +2,9 @@ from setuptools import setup, find_packages
 
 setup(name='contools',
       version='0.1',
-      description='tools for analyzing connectomics data, focus on Drosophila larva dataset',
+      description='tools for analyzing connectomics data from CATMAID instances',
       url='http://github.com/mwinding/connectome_tools',
-      author='Michael Winding and Ben Pedigo',
+      author='Michael Winding',
       author_email='mwinding@alumni.nd.edu',
       license='MIT',
       packages=find_packages(include=['contools', 'contools.*']),
